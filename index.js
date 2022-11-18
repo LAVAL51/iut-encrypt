83f1c7156b108bb68af559b1f7a8085c04726fb2
Anthony LAVAL <anthonylaval946@gmail.com>
2022-11-18 15:42:30 +0100

    Ajout de la fonction de hash

---
+++ index.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+
+exports.hash = function(string) {
+  const salt = 10;
+  return bcrypt.hash(string, salt)
+}
+
+exports.compare = function(enteredString, encryptedString) {
+  return bcrypt.compare(enteredString, encryptedString);
+}
